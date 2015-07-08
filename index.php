7be283e634f72b0c80c6f810c70045a2df9a68dd
William Bello <willjobs94@gmail.com>
2015-07-07 22:38:10 -0400

    add navbar test

--- index.php
+++ index.php
@@ -8,4 +8,17 @@
 	    <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
 	    <script type="text/javascript" src="js/materialize.min.js"></script>
 	</head>
+	<body>
+		<div class="navbar-fixed">
+    		<nav>
+	      		<div class="nav-wrapper">
+	        		<a href="#!" class="brand-logo">Logo</a>
+	       			 <ul class="right hide-on-med-and-down">
+	          			<li><a href="sass.html">Sass</a></li>
+	          			<li><a href="badges.html">Components</a></li>
+	        		</ul>
+	      		</div>
+    		</nav>
+  		</div>
+	</body>
 </html>
