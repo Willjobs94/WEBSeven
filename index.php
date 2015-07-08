2f6961556d21aaa18bc95630a1cc66312fcba2d7
William Bello <willjobs94@gmail.com>
2015-07-07 22:32:22 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width initial-scale=1"> 
+		<title> Tarea 8 y 9</title>
+	    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	    <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+	    <script type="text/javascript" src="js/materialize.min.js"></script>
+	</head>
+</html>
